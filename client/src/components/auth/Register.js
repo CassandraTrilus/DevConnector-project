f941dc9ba705792f504087b2a45a48d9abbd74f4
Cassandra Trilus <cassandratrilus@gmail.com>
2021-09-22 16:41:14 -0400

    delete functionality and Login/Register components

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -13,35 +14,39 @@ const Register = () => {
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
 
-  const onSubmit = async e => {
+  const onSubmit = async (e) => {
     e.preventDefault()
 
-    if(password !== password2) {
+    if (password !== password2) {
       console.log('Passwords do not match')
     } else {
-      const newUser = {
-        name,
-        email,
-        password,
-      }
 
-      try {
-        const config = {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
+      console.log('Success')
 
-        const body = JSON.stringify(newUser)
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password,
+      // }
 
-        const res = await axios.post('/api/users', body, config)
-        console.log(res.data)
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json'
+      //     }
+      //   }
 
-      } catch(err) {
-        console.error(err.response.data)
-      }
+      //   const body = JSON.stringify(newUser)
+
+      //   const res = await axios.post('/api/users', body, config)
+      //   console.log(res.data)
+
+      // } catch(err) {
+      //   console.error(err.response.data)
+      // }
     }
   }
+
 
   return (
     <Fragment>
@@ -94,7 +99,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
   </Fragment>
   )
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -154,13 +154,70 @@ router.delete('/', auth, async (req, res) => {
 
     // Remove Profile
     await Profile.findOneAndRemove({ user: req.user_id })
+
     // Remove User
     await User.findOneAndRemove({ _id: req.user_id })
     res.json({ msg: 'User Deleted'})
+    
   } catch(err) {
     console.error(err.message)
     res.status(500).send('Server Error')
   }
 })
+
+// @Route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+
+router.put('/experience', 
+  [ auth, 
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty()
+    ] 
+  ], 
+  async (req, res) => {
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body
+
+    const newExp = {
+      title,
+      company,
+      location, 
+      from,
+      to, 
+      current,
+      description
+    }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id })
+
+      profile.experience.unshift(newExp)
+
+      await profile.save()
+
+      res.json(profile)
+
+    } catch(err) {
+      console.error(err.message)
+      res.status(500).send('Server Error')
+    }
+  }
+)
 
 module.exports = router
